d1966093ec5e91a5da673998da52a4d9a81c1d11
vctrhugoop <vhdestroy@hotmail.com>
2024-02-23 21:53:00 -0300

    ✨ feat: add date list coffees

---
+++ src/database/coffee.ts
@@ -0,0 +1,132 @@
+export const coffees = [
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional'],
+    name: 'Expresso Tradicional',
+    description: 'O tradicional café feito com água quente e grãos moídos',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/be1d5bdf-f29f-4a04-9015-01c149f8b862-164ncr.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional'],
+    name: 'Expresso Americano',
+    description: 'Expresso diluído, menos intenso que o tradicional',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/2ce7b82d-c726-4b51-a7e5-39fd0a545159-bvpf4x.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional'],
+    name: 'Expresso Cremoso',
+    description: 'Café expresso tradicional com espuma cremosa',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/7cb6928e-5d9a-4cd2-acad-3c207c4e1055-8vsk2y.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional', 'gelado'],
+    name: 'Expresso Gelado',
+    description: 'Bebida preparada com café expresso e cubos de gelo',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/08aa4928-a6aa-49a4-a784-4fec47acd2c9-zaomey.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional', 'com leite'],
+    name: 'Café com Leite',
+    description: 'Meio a meio de expresso tradicional com leite vaporizado',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/16ed6853-5340-4723-89aa-b61ac2493080-c8cy0q.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional', 'com leite'],
+    name: 'Latte',
+    description:
+      'Uma dose de café expresso com o dobro de leite e espuma cremosa',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/3b75649a-8fe5-456e-add0-bef131b50c05-1lh0k3.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional', 'com leite'],
+    name: 'Capuccino',
+    description:
+      'Bebida com canela feita de doses iguais de café, leite e espuma',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/ed94f613-cbd3-45d0-88a3-03b0e29dcff6-jj5290.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional', 'com leite'],
+    name: 'Macchiato',
+    description:
+      'Café expresso misturado com um pouco de leite quente e espuma',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/abec8160-43fe-405a-92b2-4e5316c2f58c-ikozac.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['tradicional', 'com leite'],
+    name: 'Mocaccino',
+    description: 'Café expresso com calda de chocolate, pouco leite e espuma',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/dce0bb5a-f078-4d24-ac63-e44a4538c3ab-e809lf.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['especial', 'com leite'],
+    name: 'Chocolate Quente',
+    description: 'Bebida feita com chocolate dissolvido no leite quente e café',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/b292366b-fb76-446d-9fdd-1c26afb4c9d5-9rrao5.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['especial', 'gelado', 'alcoólico'],
+    name: 'Cubano',
+    description:
+      'Drink gelado de café expresso com rum, creme de leite e hortelã',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/13f2fd13-4c6e-474b-ac17-ceaabfba1ac3-hkpzg5.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['especial'],
+    name: 'Havaiano',
+    description: 'Bebida adocicada preparada com café e leite de coco',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/aea3fe20-a604-4dba-bdf9-6ef07ae9b6ef-d3xy80.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['especial'],
+    name: 'Árabe',
+    description: 'Bebida preparada com grãos de café árabe e especiarias',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/b0a3cd2e-94de-4aec-8682-6e5b6a3ce26e-75d4g.png',
+  },
+  {
+    id: crypto.randomUUID(),
+    tags: ['especial', 'alcoólico'],
+    name: 'Irlandês',
+    description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
+    price: 9.9,
+    imageURL:
+      'https://utfs.io/f/955e1e45-b906-4a12-b5da-2a282a3ba9c7-nmdr34.png',
+  },
+];

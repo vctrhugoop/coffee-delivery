83d0890c6f99c1ae1fef1cdc3d4b3f91e5d22397
vctrhugoop <vhdestroy@hotmail.com>
2024-02-24 12:32:48 -0300

    ✨ feat:

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -8,18 +8,18 @@ interface ButtonContainerProps {
 
 const buttonVariant = {
   primary: css`
-    background-color: ${({ theme }) => theme['yellow-500']};
+    background-color: ${({ theme }) => theme.colors['yellow-500']};
     padding: 1.2rem 0.8rem;
 
     text-transform: uppercase;
 
     &:hover {
-      background-color: ${({ theme }) => theme['yellow-700']};
+      background-color: ${({ theme }) => theme.colors['yellow-700']};
     }
   `,
   secundary: css`
-    background-color: ${({ theme }) => theme['base-button']};
-    color: ${({ theme }) => theme['base-text']};
+    background-color: ${({ theme }) => theme.colors['base-button']};
+    color: ${({ theme }) => theme.colors['base-text']};
     font-size: 1.2rem;
 
     padding: 0 0.8rem;
@@ -30,22 +30,22 @@ const buttonVariant = {
     text-transform: uppercase;
 
     &:hover {
-      background-color: ${({ theme }) => theme['base-hover']};
-      color: ${({ theme }) => theme['base-subtitle']};
+      background-color: ${({ theme }) => theme.colors['base-hover']};
+      color: ${({ theme }) => theme.colors['base-subtitle']};
     }
   `,
   icon: css`
-    background-color: ${({ theme }) => theme['purple-700']};
+    background-color: ${({ theme }) => theme.colors['purple-700']};
     padding: 0.8rem;
 
     &:hover {
-      background-color: ${({ theme }) => theme['purple-500']};
+      background-color: ${({ theme }) => theme.colors['purple-500']};
     }
   `,
 };
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-  color: ${({ theme }) => theme.white};
+  color: ${({ theme }) => theme.colors['base-white']};
 
   border-radius: 6px;
 

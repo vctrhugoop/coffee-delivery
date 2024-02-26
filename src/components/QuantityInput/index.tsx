6fadc5472b31b849cf0fac2e7b5a0063b44c1794
vctrhugoop <vhdestroy@hotmail.com>
2024-02-26 14:23:10 -0300

    ✨ feat: add initial function increment and decrement quantity coffee

--- src/components/QuantityInput/index.tsx
+++ src/components/QuantityInput/index.tsx
@@ -1,14 +1,25 @@
 import { Minus, Plus } from '@phosphor-icons/react';
+import { useState } from 'react';
 import { IconWrapper, QuantityInputContainer } from './styles';
 
 export function QuantityInput() {
+  const [quantity, setQuantity] = useState(1);
+
+  function handleDecrementQuantity() {
+    setQuantity(quantity - 1);
+  }
+
+  function handleIncrementQuantity() {
+    setQuantity(quantity + 1);
+  }
+
   return (
     <QuantityInputContainer>
-      <IconWrapper>
+      <IconWrapper onClick={handleDecrementQuantity} disabled={quantity <= 1}>
         <Minus />
       </IconWrapper>
-      <input type='number' readOnly value={1} />
-      <IconWrapper>
+      <input type='number' readOnly value={quantity} />
+      <IconWrapper onClick={handleIncrementQuantity}>
         <Plus />
       </IconWrapper>
     </QuantityInputContainer>

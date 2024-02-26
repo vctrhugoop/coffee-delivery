b2718585772d2625ea7b2692b8214bb0bf3c0b68
vctrhugoop <vhdestroy@hotmail.com>
2024-02-26 08:08:34 -0300

    ✨ feat: create initial setup bagde quatity cart component

--- src/components/Header/components/CartButton/index.tsx
+++ src/components/Header/components/CartButton/index.tsx
@@ -1,10 +1,11 @@
 import { ShoppingCart } from '@phosphor-icons/react';
-import { CartButtonContainer } from './style';
+import { BagdeQuantityCart, CartButtonContainer } from './style';
 
 export function CartButton() {
   return (
     <CartButtonContainer>
       <ShoppingCart size={22} weight='fill' />
+      <BagdeQuantityCart>2</BagdeQuantityCart>
     </CartButtonContainer>
   );
 }
--- src/components/Header/components/CartButton/style.ts
+++ src/components/Header/components/CartButton/style.ts
@@ -11,4 +11,27 @@ export const CartButtonContainer = styled.button`
   svg {
     color: ${({ theme }) => theme.colors['yellow-700']};
   }
+
+  position: relative;
+`;
+
+export const BagdeQuantityCart = styled.div`
+  position: absolute;
+  top: -0.8rem;
+  right: -0.8rem;
+
+  width: 2rem;
+  height: 2rem;
+
+  background-color: ${({ theme }) => theme.colors['yellow-700']};
+  border-radius: 50%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors['base-white']};
 `;

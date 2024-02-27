d381ff88a4087d9bff0f418c1ec035a93c8a68c0
vctrhugoop <vhdestroy@hotmail.com>
2024-02-27 20:58:50 -0300

    ✨ feat: add pages in router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { DefaultLayout } from './layouts/DefaultLayout';
+import { CheckoutOrder } from './pages/CheckoutOrder';
+import { FinishedOrder } from './pages/FinishedOrder';
 import { Home } from './pages/Home';
 
 export function Router() {
@@ -8,6 +10,8 @@ export function Router() {
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
         <Route path='/' element={<Home />} />
+        <Route path='/checkout-order' element={<CheckoutOrder />} />
+        <Route path='/finished-order' element={<FinishedOrder />} />
       </Route>
     </Routes>
   );

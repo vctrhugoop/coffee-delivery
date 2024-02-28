bfab40ee710fdf8f230a6c520099624e3224965c
vctrhugoop <vhdestroy@hotmail.com>
2024-02-28 20:02:55 -0300

    ✨ feat: show quantity coffe in cart

--- src/components/Header/components/CartButton/index.tsx
+++ src/components/Header/components/CartButton/index.tsx
@@ -1,13 +1,18 @@
 import { ShoppingCart } from '@phosphor-icons/react';
 import { NavLink } from 'react-router-dom';
+import { useCart } from '../../../../hooks/useCart';
 import { BagdeQuantityCart, CartButtonContainer } from './style';
 
 export function CartButton() {
+  const { cartQuantity } = useCart();
+
   return (
     <NavLink to='/checkout-order'>
       <CartButtonContainer>
         <ShoppingCart size={22} weight='fill' />
-        <BagdeQuantityCart>2</BagdeQuantityCart>
+        {cartQuantity >= 1 && (
+          <BagdeQuantityCart>{cartQuantity}</BagdeQuantityCart>
+        )}
       </CartButtonContainer>
     </NavLink>
   );

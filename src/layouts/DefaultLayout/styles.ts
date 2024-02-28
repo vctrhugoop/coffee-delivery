1bf08865b381e27c3eaa0d8dbc042bc1369a5a00
vctrhugoop <vhdestroy@hotmail.com>
2024-02-27 21:00:55 -0300

    🌈 style: add sr only class

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -6,4 +6,16 @@ export const LayoutContainer = styled.div`
     max-width: 112rem;
     margin: 0 auto;
   }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
 `;
